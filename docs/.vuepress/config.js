fa2ea49a1a1e111ccfc0f373debd8ebbfe932d52
Flexfinancial <Cynthiatk7@gmail.com>
2022-07-30 17:59:14 +0800

    update nav

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,14 +1,18 @@
 module.exports = {
   title: 'CityFlySky',
   description: 'Fund your thing with CityFlySky. The funding platform for DAOs, decentralized crowdfunding, Web3 businesses and communities. Built on Chain.',
-
+  themeConfig: {
+	logo: '/assets/img/logo.png',
+	navbar:[
+	  {
+		  text:"Intro",
+		  link:'/'
+	  }
+	],
+  },
+/*
   head: [
     ['link', { rel: 'shortcut icon', type: "image/x-icon", href: `/favicon.ico` }]
-  ],
-  themeConfig: {
-
-    
-  },
-
-
+  ]
+*/
 }
